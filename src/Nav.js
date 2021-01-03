da01d7919da14cba5bd984cdd7e5530489cd0267
AE <wkddnjsdlr1@gmail.com>
2021-01-04 04:16:24 +0900

    netflix-clone base

---
+++ src/Nav.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import './Nav.css';
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+          handleShow(true);
+        } else handleShow(false);
+      });
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://a.ripple.moe/53212?0"
+        alt="AVATAR"
+      />
+    </div>
+  );
+}
+
+export default Nav;
